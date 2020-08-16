67e4364c62b1382fe378689103f9dd03819a43dd
Josh Raiborde <joshsrai@yahoo.com>
2020-08-16 22:10:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,9 @@ class Song
     artist.add_song(self)
   end
   
-    def genre=(genre)
-    @genre = genre
-    genre.add_song(self)
-  end
+  #   def genre=(genre)
+  #   @genre = genre
+  #   genre.add_song(self)
+  # end
   
 end
