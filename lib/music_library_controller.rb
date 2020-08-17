f19469393a4e9bb81bd02e040b05ad6d438f766b
Josh Raiborde <joshsrai@yahoo.com>
2020-08-17 01:28:10 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -45,9 +45,10 @@ class MusicLibraryController
       
       if artist = Artist.find_by_nme(input)
         artist.songs.sort_by(&:name).each.with_index(1) do |song, idx|
-        puts "#{idx}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        puts "#{idx}. #{song.name} - #{song.genre.name}"
       end
-      
+    end
+    end
     end
     
 
