1a665d032e0701306d0113268f620ae5031e055b
Josh Raiborde <joshsrai@yahoo.com>
2020-08-17 01:20:05 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,7 +29,7 @@ class MusicLibraryController
     
     def list_artists
       Artist.all.sort_by(&:name).each.with_index(1) do |art, idx|
-        puts "#{idx}. #{art.name}""
+        puts "#{idx}. #{art.name}"
       end
     end
 
